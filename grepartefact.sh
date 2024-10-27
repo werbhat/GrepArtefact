4455199e1f07f654ccd0cb69b854dd6246d008b9
werbhat <32008039+werbhat@users.noreply.github.com>
2024-10-27 08:01:03 +0100

    Update grepartefact.sh
    Updated to better manage parsers

--- grepartefact.sh
+++ grepartefact.sh
@@ -14,19 +14,21 @@ NC='\e[0m' # No Color
 echo -e "${YELLOW} - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ${NC}"
 figlet -k WerbHat
 echo ""
-echo "Version 2.0"
+echo "Version 2.1"
 echo ""
 echo -e "${YELLOW} With this script, we can find artefact in the file."
 echo -e "${YELLOW} For example, you can find IPv4 address, email, domain"
-echo -e "${YELLOW} Command: ./grepartefact.sh file -ip4 -dom"
-echo -e "${RED}   -help for display available options"
+echo -e "${YELLOW} Command: ./grepartefact.sh -f file -ip4 -dom -o output_report.txt"
+echo -e "${BLUE}   -help for display available options"
 echo -e "${YELLOW} - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ${NC}"
 
 # User guide
 usage() {
-    echo -e "${GREEN}Usage:${NC} $0 <file> <options...> [-o output_file]"
+    echo -e "${GREEN}Usage:${NC} $0 -f <file> <options...> [-o output_file]"
     echo -e "${BLUE}Available Options:${NC}"
+    echo -e "  ${YELLOW}-all${NC}    All of the above"
     echo -e "  ${YELLOW}-ip4${NC}    IPv4 Addresses"
+    echo -e "  ${YELLOW}-ip6${NC}    IPv6 Addresses"
     echo -e "  ${YELLOW}-email${NC}  Email Addresses"
     echo -e "  ${YELLOW}-dom${NC}    Domain Names"
     echo -e "  ${YELLOW}-url${NC}    URLs"
@@ -34,24 +36,50 @@ usage() {
     echo -e "  ${YELLOW}-phone${NC}  Phone Numbers"
     echo -e "  ${YELLOW}-dateUS${NC} Dates (ISO 8601)"
     echo -e "  ${YELLOW}-dateFR${NC} Dates (French)"
-    echo -e "  ${YELLOW}-ip6${NC}    IPv6 Addresses"
     echo -e "  ${YELLOW}-www${NC}    URLs beginning with www"
-    echo -e "  ${YELLOW}-all${NC}    All of the above"
     exit 1
 }
+
+file=""
+file_report=""
+search_all=false
 
 if [ -z "$1" ]; then
     usage
 fi
 
-if [[ "$1" == "-help" ]]; then
+while (( "$#" )); do
+    case "$1" in
+        -help)
+            usage
+            ;;
+        -f)
+            shift
+            file=$1
+            ;;
+        -o)
+            shift
+            file_report=$1
+            ;;
+        -all)
+            search_all=true
+            ;;
+        -ip4|-email|-dom|-url|-mac|-phone|-dateUS|-dateFR|-ip6|-www)
+            options+=("$1")
+            ;;
+        *)
+            echo -e "${RED}Invalid option: $1${NC}" >&2
+            usage
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$file" ]; then
+    echo -e "${RED}Error: Input file not specified. Use -f to specify the file.${NC}" >&2
     usage
 fi
 
-file=$1
-shift
-file_report=""
-search_all=false
 declare -A patterns=(
     ["-ip4"]="IPv4 Addresses:|([0-9]{1,3}\.){3}[0-9]{1,3}"
     ["-email"]="Email Addresses:|[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}"
@@ -65,58 +93,28 @@ declare -A patterns=(
     ["-www"]="URLs beginning with www:|www\.[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}"
 )
 
-# Check for -o option for output file
-while (( "$#" )); do
-    if [[ "$1" == "-o" ]]; then
-        shift
-        file_report=$1
-        shift
-    else
-        break
-    fi
-done
-
 if [ -n "$file_report" ]; then
     echo -e "${BLUE}Analysis report of file $file${NC}" > $file_report
     echo -e "${BLUE}Date:${NC} $(date)" >> $file_report
     echo "" >> $file_report
 fi
 
-while (( "$#" )); do
-    if [[ "$1" == "-all" ]]; then
-        search_all=true
-    elif [[ -n "${patterns[$1]}" ]]; then
-        if [ -n "$file_report" ]; then
-            echo -e "${YELLOW}${patterns[$1]%%|*}${NC}" >> $file_report
-            grep -Eo "${patterns[$1]#*|}" $file >> $file_report
-            echo "" >> $file_report
-        else
-            echo -e "${YELLOW}${patterns[$1]%%|*}${NC}"
-            grep -Eo "${patterns[$1]#*|}" $file
-            echo ""
-        fi
-    else
-        echo -e "${RED}Invalid option: $1${NC}" >&2
-        usage
-    fi
-    shift
-done
-
 if $search_all; then
-    for key in "${!patterns[@]}"; do
-        if [ -n "$file_report" ]; then
-            echo -e "${YELLOW}${patterns[$key]%%|*}${NC}" >> $file_report
-            grep -Eo "${patterns[$key]#*|}" $file >> $file_report
-            echo "" >> $file_report
-        else
-            echo -e "${YELLOW}${patterns[$key]%%|*}${NC}"
-            grep -Eo "${patterns[$key]#*|}" $file
-            echo ""
-        fi
-    done
+    options=("-ip4" "-email" "-dom" "-url" "-mac" "-phone" "-dateUS" "-dateFR" "-ip6" "-www")
 fi
+
+for opt in "${options[@]}"; do
+    if [ -n "$file_report" ]; then
+        echo -e "${YELLOW}${patterns[$opt]%%|*}${NC}" >> $file_report
+        grep -Eo "${patterns[$opt]#*|}" $file >> $file_report
+        echo "" >> $file_report
+    else
+        echo -e "${YELLOW}${patterns[$opt]%%|*}${NC}"
+        grep -Eo "${patterns[$opt]#*|}" $file
+        echo ""
+    fi
+done
 
 if [ -n "$file_report" ]; then
     cat $file_report
 fi
-
